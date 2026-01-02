024f5ca3ba9153a7718a396e08a9cb3e0cc63d2e
Emily McLean <emilymcleandeveloper@gmail.com>
2026-01-02 19:24:01 +1100

    Add location field to scraped event sites without them

--- scraper/gang_gang_scraper.py
+++ scraper/gang_gang_scraper.py
@@ -12,4 +12,6 @@ class GangGangScraper(ICalScraper):
         return "https://ganggangcafe.com.au/events/?ical=1"
 
     def _modify(self, event: Event) -> Event:
+        if event.get("location") is None:
+            event.add("location", "Gang Gang Cafe, Shop 4/2 Frencham Pl, Downer ACT 2602")
         return event
