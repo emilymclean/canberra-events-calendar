0d6ea8e167c517b55116cd71df2971ba00f7e027
Emily McLean <emilymcleandeveloper@gmail.com>
2026-01-02 19:25:05 +1100

    Just add location

--- scraper/gang_gang_scraper.py
+++ scraper/gang_gang_scraper.py
@@ -12,6 +12,5 @@ class GangGangScraper(ICalScraper):
         return "https://ganggangcafe.com.au/events/?ical=1"
 
     def _modify(self, event: Event) -> Event:
-        if event.get("location") is None:
-            event.add("location", "Gang Gang Cafe, Shop 4/2 Frencham Pl, Downer ACT 2602")
+        event.add("location", "Gang Gang Cafe, Shop 4/2 Frencham Pl, Downer ACT 2602")
         return event
