ee28a6972848d8afba51d1d4907bb0f0529f87a3
Emily McLean <emilymcleandeveloper@gmail.com>
2026-01-03 19:55:37 +1100

    Add keep-alive to workflow

--- scraper/smiths_scraper.py
+++ scraper/smiths_scraper.py
@@ -14,7 +14,7 @@ class SmithsAlternativeScraper(ICalScraper):
         return "https://www.smithsalternative.com/infomaxim/api/v1/events/search.ics"
 
     def _modify(self, event: Event) -> Event:
-        event.uid = f'{self.id()}-{hashlib.md5(f"{event.name}{event.DTSTART}{event.DTEND}{event.DTSTAMP}".encode()).hexdigest()}'
+        event.uid = f'{self.id()}-{hashlib.md5(f"{event.name}{event.DTSTART}{event.DTEND}".encode()).hexdigest()}'
         event.pop("location")
         event.add("location", "Smiths Alternative, 76 Alinga St, Canberra ACT 2601")
         return event
